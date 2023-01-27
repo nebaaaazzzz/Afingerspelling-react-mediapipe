4ff7777417748dd38214038e77e958c7a828772b
Afiya21 <NebyatAhmed21@gmail.com>
2023-01-27 08:49:28 -0500

    improving bgand fonts

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { ScaleLoader } from "react-spinners";
 function Loading() {
   return (
-    <div className="h-[100vh] w-full gap-20 flex flex-col justify-center items-center ">
+   
+    <div className="h-[100vh] w-full gap-20 flex flex-col justify-center items-center bg-[#683aff]  bg-[url('./spelling/Q.png')]
+     bg-auto bg-no-repeat bg-center ">
       <ScaleLoader color={"#000"} />
       <p className="text-center">
         Please Sleep While Setting Up It May Take a While
