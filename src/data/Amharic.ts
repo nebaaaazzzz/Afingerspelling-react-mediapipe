9fcdef80302963d6bf8e9ac6b03fbe763ffbdf49
Afiya21 <NebyatAhmed21@gmail.com>
2023-03-18 03:09:50 -0400

    adding the first 5 letter of amharic in Ai

---
+++ src/data/Amharic.ts
@@ -0,0 +1,246 @@
+import { AlphabetDefinationI } from '../type';
+
+export class Alphabet {
+  private alphabetArray: AlphabetDefinationI[] = [];
+
+  private _helpTexts = { straight: 'Straigthen it', bend: 'Bend it' };
+
+  constructor() {
+    let noCurl = 180;
+    let halfCurlMin = 130;
+    let fullCurlMinMoreForgiving = 90;
+    let fullCurlMin = 60;
+    let fullCurlMax = 0;
+
+    let ሀ: AlphabetDefinationI = {
+      letterNumber: 1,
+      letter: 'ሀ',
+      rotation: 'up',
+      thumb: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'thumbBendOverOtherFingers',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      index: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.straight
+      },
+      middle: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.straight
+      },
+      ring: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.straight
+      },
+      little: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.straight
+      }
+    };
+    let ለ: AlphabetDefinationI = {
+      letterNumber: 1,
+      letter: 'ለ',
+      rotation: 'up',
+      thumb: {
+        curlMin: 180,
+        curlMax: 110,
+        special: 'pinchThumbAndPointer',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: 'Pinch the thumb and pointer finger together'
+      },
+      index: {
+        curlMin: noCurl,
+        curlMax: fullCurlMax,
+        special: 'pinchThumbAndPointer',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.bend
+      },
+      middle: {
+        curlMin: fullCurlMinMoreForgiving,
+        curlMax: fullCurlMax,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.bend
+      },
+      ring: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.straight
+      },
+      little: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.straight
+      }
+    };
+    let ሐ: AlphabetDefinationI = {
+      letterNumber: 3,
+      letter: 'ሐ',
+      rotation: 'up',
+      thumb: {
+        curlMin: noCurl,
+        curlMax: fullCurlMax,
+        special: 'thumbBendOverOtherFingers',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      index: {
+        curlMin: fullCurlMinMoreForgiving,
+        curlMax: fullCurlMax,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      middle: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.straight
+      },
+      ring: {
+        curlMin: fullCurlMinMoreForgiving,
+        curlMax: fullCurlMax,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      little: {
+        curlMin: fullCurlMinMoreForgiving,
+        curlMax: fullCurlMax,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null
+      }
+    };
+    let መ: AlphabetDefinationI = {
+      letterNumber: 14,
+      letter: 'መ',
+      rotation: 'up',
+      thumb: {
+        curlMin: noCurl,
+        curlMax: fullCurlMax,
+        special: 'betweenMiddleAndRing',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      index: {
+        curlMin: fullCurlMinMoreForgiving,
+        curlMax: fullCurlMax,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      middle: {
+        curlMin: fullCurlMinMoreForgiving,
+        curlMax: fullCurlMax,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      ring: {
+        curlMin: fullCurlMinMoreForgiving,
+        curlMax: fullCurlMax,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      little: {
+        curlMin: fullCurlMinMoreForgiving,
+        curlMax: fullCurlMax,
+        special: 'none',
+        percentageCorrect: 0,
+        currentAngle: null
+      }
+    };
+    let ሰ: AlphabetDefinationI = {
+      letterNumber: 5,
+      letter: 'ሰ',
+      rotation: 'up',
+      thumb: {
+        curlMin: 180,
+        curlMax: 110,
+        special: 'pinchThumbAndPointer',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: 'Pinch the thumb and pointer finger together'
+      },
+      index: {
+        curlMin: noCurl,
+        curlMax: fullCurlMax,
+        special: 'pinchThumbAndPointer',
+        percentageCorrect: 0,
+        currentAngle: null,
+        helpText: this._helpTexts.bend
+      },
+      middle: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'littleAndMiddleAndRingMustBeApart',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      ring: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'littleAndMiddleAndRingMustBeApart',
+        percentageCorrect: 0,
+        currentAngle: null
+      },
+      little: {
+        curlMin: noCurl,
+        curlMax: halfCurlMin,
+        special: 'littleAndMiddleAndRingMustBeApart',
+        percentageCorrect: 0,
+        currentAngle: null
+      }
+    };
+
+    this.alphabetArray = [];
+  }
+
+  public getRandomLetter = () => {
+    var getRandom = Math.floor(Math.random() * this.alphabetArray.length);
+    return this.alphabetArray[getRandom];
+  };
+
+  public getSpecificLetter = (findLetter: string) => {
+    var length = this.alphabetArray.length;
+    for (var i = 0; i < length; i++) {
+      var currentLetter = this.alphabetArray[i];
+      var getLetterName = currentLetter.letter;
+      if (getLetterName.toLowerCase() === findLetter.toLowerCase()) {
+        return currentLetter;
+      }
+    }
+  };
+}
